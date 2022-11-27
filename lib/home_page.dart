02425d23faea33eb6577f457b468e39a46e0e474
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-11-27 16:31:26 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   elevation: 0,
+        //   actions: const [
+        //     Padding(
+        //       padding: EdgeInsets.only(right: 20),
+        //       child: Icon(
+        //         Icons.notifications_outlined,
+        //         color: Colors.grey,
+        //       ),
+        //     )
+        //   ],
+        // ),
+        // drawer: Drawer(
+        //   backgroundColor: Colors.grey,
+        // ),
+        body: Text('dhghfdgf'),
+      ),
+    );
+  }
+}
